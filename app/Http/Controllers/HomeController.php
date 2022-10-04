7f2082fe2a381d02b2e70a8754f2dd6f8122cc44
Wagner Bugs <wagnerbugs@gmail.com>
2022-10-04 15:49:45 -0300

    route controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,7 +31,7 @@ class HomeController extends Controller
     public function contactDestroy(Contato $contato)
     {
         $contato->delete();
-        return route('home');
+        return redirect()->route('home');
     }
 
 
