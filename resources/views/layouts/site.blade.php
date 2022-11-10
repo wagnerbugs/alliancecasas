a9e654b26e054d4b9a9f0daeb4a6b4727cb26089
Wagner Bugs <wagnerbugs@gmail.com>
2022-11-10 16:11:25 -0300

    pixel facebook

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -38,6 +38,23 @@
     <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=6322ed91560f0d00125e7715&product=sop' async='async'></script>
 
 
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '712230143826763');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=712230143826763&ev=PageView&noscript=1" /></noscript>
+    <!-- End Meta Pixel Code -->
+
 
     @yield('codeHead')
 </head>
